cbbed2c5245b7233b2417f314c7f1e326fd778f5
Ross <rdtredinnick@wisc.edu>
2021-05-21 22:30:45 -0500

    Corrected hiding flippers when losing tracking after upgrading unity integration

--- Assets/Oculus/VR/Scripts/Util/OVRHand.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRHand.cs
@@ -76,6 +76,15 @@ public class OVRHand : MonoBehaviour,
 	private void Update()
 	{
 		GetHandState(OVRPlugin.Step.Render);
+		
+		if(IsDataHighConfidence)
+		{
+			transform.GetChild(0).gameObject.SetActive(true);
+		}
+		else
+		{
+			transform.GetChild(0).gameObject.SetActive(false);
+		}
 	}
 
 	private void FixedUpdate()
