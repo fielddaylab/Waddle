bdc3d80898bcc7cc8baad4b7ecd777524b0b7828
Ross <rdtredinnick@wisc.edu>
2021-12-01 23:04:37 -0600

    Environment tweaks

--- Assets/Scripts/FeetRaycast.cs
+++ Assets/Scripts/FeetRaycast.cs
@@ -18,7 +18,7 @@ public class FeetRaycast : MonoBehaviour
 	Vector3 _rotationToApply;
 	Vector3 RotationToApply => _rotationToApply;
 	
-	Vector3 _lastPosition;
+	Vector3 _lastPosition = Vector3.zero;
 	
 	float _penguinCapsuleHeight = 0.7112f;
 	
@@ -37,39 +37,42 @@ public class FeetRaycast : MonoBehaviour
 
 	void LateUpdate()
 	{
-		RaycastHit hitInfo;
-		
-		if(Physics.Raycast(_eyeObject.transform.position, Vector3.down, out hitInfo, Mathf.Infinity, _layerMask, QueryTriggerInteraction.Ignore))
+		//if(Vector3.Distance(_eyeObject.transform.position, _lastPosition) > 0.1f)
 		{
-			//Debug.Log("Hit point: " + hitInfo.point);
-			Vector3 pos = transform.position;
-			pos.x = hitInfo.point.x;
-			pos.z = hitInfo.point.z;
-			//pos.y = hitInfo.point.y;//_eyeObject.transform.position.y - _penguinCapsuleHeight + (_penguinCapsuleHeight - _eyeObject.transform.localPosition.y);	//also take into account difference between tracked height and capsule height...
-			transform.position = pos;
+			RaycastHit hitInfo;
+			
+			if(Physics.Raycast(_eyeObject.transform.position, Vector3.down, out hitInfo, Mathf.Infinity, _layerMask, QueryTriggerInteraction.Ignore))
+			{
+				//Debug.Log("Hit point: " + hitInfo.point);
+				Vector3 pos = transform.position;
+				pos.x = hitInfo.point.x;
+				pos.z = hitInfo.point.z;
+				//pos.y = hitInfo.point.y;//_eyeObject.transform.position.y - _penguinCapsuleHeight + (_penguinCapsuleHeight - _eyeObject.transform.localPosition.y);	//also take into account difference between tracked height and capsule height...
+				transform.position = pos;
 
-			Vector3 flatForward = _eyeObject.transform.forward;
-			flatForward.y = 0f;
-			flatForward = flatForward.normalized;
+				Vector3 flatForward = _eyeObject.transform.forward;
+				flatForward.y = 0f;
+				flatForward = flatForward.normalized;
 
-			//Jack's changes
-			Quaternion q = Quaternion.identity;
-			q.eulerAngles = _rotationToApply;
-		
-			if( Vector3.Dot(_eyeObject.transform.up, Vector3.down) > 0){
-				Vector3 tmp = q.eulerAngles;
-				tmp.y = q.eulerAngles.y - 180;
-				q.eulerAngles = tmp;
-				transform.position += (flatForward * 0.15f);
+				//Jack's changes
+				Quaternion q = Quaternion.identity;
+				q.eulerAngles = _rotationToApply;
+			
+				if( Vector3.Dot(_eyeObject.transform.up, Vector3.down) > 0){
+					Vector3 tmp = q.eulerAngles;
+					tmp.y = q.eulerAngles.y - 180;
+					q.eulerAngles = tmp;
+					transform.position += (flatForward * 0.15f);
+				}
+				else{
+					transform.position -= (flatForward * 0.1f);	//this should be related to scale
+				}
+
+				transform.rotation = _rotationTransform.transform.rotation;
+				transform.rotation *= q;
 			}
-			else{
-				transform.position -= (flatForward * 0.1f);	//this should be related to scale
-			}
-
-			transform.rotation = _rotationTransform.transform.rotation;
-			transform.rotation *= q;
+			
+			_lastPosition = _eyeObject.transform.position;
 		}
-		
-		_lastPosition = _eyeObject.transform.position;
 	}
 }
